d582de5a256b60507dddec6ab86affea64ad9f85
Matt Baker <matthew.baker@mikesierra.net>
2024-06-24 18:23:07 +0100

    add timestamps to seeder

--- ocsi_test_backend/database/seeders/MessagesSeeder.php
+++ ocsi_test_backend/database/seeders/MessagesSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -15,18 +16,28 @@ class MessagesSeeder extends Seeder
     {
         DB::table('messages')->insert([
             'message' => 'Hello World',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('messages')->insert([
             'message' => 'Bom Dia',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('messages')->insert([
             'message' => 'Buen día',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('messages')->insert([
             'message' => 'Bonne journée',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('messages')->insert([
             'message' => 'Guten Tag',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
     }
 }
